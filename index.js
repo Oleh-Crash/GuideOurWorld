b22bfa3b97b74d099554924123799f8f5584a63b
Oleh-Crash <olezhkanovikov29@gmail.com>
2020-10-08 19:03:03 +0000

    Comment

--- index.js
+++ index.js
@@ -1,20 +1,151 @@
-window.onload = function(){
-   window.setInterval(
-	function(){
-	    var d = new Date();
-	    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
-	}
-  , 1000);
-}
+$(document).ready(function(){
+   
+  //function to display time in warszaw
+  function timeDisplay() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridiem = " ";
+   
+    if(hours >= 12){
+      hours = hours - 12;
+      meridiem = "pm";
+    }
+    else{
+      meridiem = "am";
+    }
+    if(hours === 0){
+      hours = 12;
+    }
+    if(hours < 10){
+        hours = "0" + hours;
+      }
+    if(minutes < 10){
+      minutes = "0" + minutes;
+    }
+    if(seconds < 10){
+      seconds = "0" + seconds;
+    }
+    
+    var clockDiv = document.getElementById('clock');
+    clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+  }
+  
+  timeDisplay();
+  setInterval(timeDisplay, 1000);
+  
 
-window.onload = function(){
-   window.setInterval(
-	function(){
-        var d = new Date();
+  
+  //function to display time in london
+  function newYorkTimeDisplay(offset) {
+    var currentTime = new Date();  
+    currentTime.setHours(currentTime.getHours()+offset); 
         
-        document.getElementById("clocklondon").innerHTML = d.toLocaleTimeString();
-       
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridiem = " ";
+   
+    if(hours >= 12){
+      hours = hours - 12;
+      meridiem = "pm";
+    }
+    else{
+      meridiem = "am";
+    }
+    if(hours === 0){
+      hours = 12;
+    }
+    if(hours < 10){
+        hours = "0" + hours;
+      }
+    if(minutes < 10){
+      minutes = "0" + minutes;
+    }
+    if(seconds < 10){
+      seconds = "0" + seconds;
+    }
+    
+    var newYorkDiv = document.getElementById('clocklondon');      
+    newYorkDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem; 
+  }
+     
+  newYorkTimeDisplay(+3);
+  setInterval(newYorkTimeDisplay, 1000, -1); 
+  
+
+//function to display time in (EST) dubai
+  function londonTimeDisplay(offset) {
+    var currentTime = new Date();  
+    currentTime.setHours(currentTime.getHours()+offset); 
         
-	}
-  , 1000);
-}
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridiem = " ";
+   
+    if(hours >= 12){
+      hours = hours - 12;
+      meridiem = "pm";
+    }
+    else{
+      meridiem = "am";
+    }
+    if(hours === 0){
+      hours = 12;
+    }
+    if(hours < 10){
+        hours = "0" + hours;
+      }
+    if(minutes < 10){
+      minutes = "0" + minutes;
+    }
+    if(seconds < 10){
+      seconds = "0" + seconds;
+    }
+    
+    var londonkDiv = document.getElementById('clockdubai');      
+    londonkDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem; 
+  }
+     
+  londonTimeDisplay(+3);
+  setInterval(londonTimeDisplay, 1000, +2); 
+  
+
+ //function to display time in berlin
+  function berlinDisplay() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridiem = " ";
+   
+    if(hours >= 12){
+      hours = hours - 12;
+      meridiem = "pm";
+    }
+    else{
+      meridiem = "am";
+    }
+    if(hours === 0){
+      hours = 12;
+    }
+    if(hours < 10){
+        hours = "0" + hours;
+      }
+    if(minutes < 10){
+      minutes = "0" + minutes;
+    }
+    if(seconds < 10){
+      seconds = "0" + seconds;
+    }
+    
+    var berlinDiv = document.getElementById('clockberlin');
+    berlinDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+  }
+  
+  berlinDisplay();
+  setInterval(berlinDisplay, 1000);
+
+});
